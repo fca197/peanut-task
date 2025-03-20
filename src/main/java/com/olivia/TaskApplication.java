e87a1f334ae3f644afccc357bc018c6b905179b4
wangbao <197235339@qq.com>
2025-03-20 18:10:21 +0800

    init  独立 task

--- src/main/java/com/olivia/TaskApplication.java
+++ src/main/java/com/olivia/TaskApplication.java
@@ -27,6 +27,7 @@ public class TaskApplication {
 
 
   static {
+    ApplicationStartCondition.setStartModelName("task");
     // 非window下 加载 or-tools 库
     // window下使用修改pom.xml中 ortools-java依赖排除的节点删除,增加window依赖
 //    log.info("load or-tools {}", SystemUtil.getOsInfo().getName());
